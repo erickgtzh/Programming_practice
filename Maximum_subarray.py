4c2ebf0fa399bff7bf81b5df9c36252c379b1bfa
erickgtzh <0193662@up.edu.mx>
2019-02-08 22:32:32 -0600

    Update Maximum_subarray.py

--- Maximum_subarray.py
+++ Maximum_subarray.py
@@ -1,17 +1,22 @@
-nums = [-2,1,-3,4,-1,2,1,-5,4]
+nums = [8,-19,5,-4,20]
 
-def maxSubArray(self,nums):
-    sum_max = 0
+def maxSubArray(nums):
     suma = 0
     mayor = -2147483647
     
+    
+    #----------------Test---------------#
+    #suma    = 8, -11, -6, 5, 1, 21
+    #sum_max = 8, -11, 5, 1, 21
+    #mayor   = 8, 21
+    # i                  .
+    #nums = [8,-19,5,-4,20]
     for i in range(len(nums)):
         suma += nums[i]
         if(nums[i]>suma):
-            suma = nums[i]           
-        sum_max = suma       
-        if(sum_max>mayor):
-            mayor = sum_max
+            suma = nums[i]   
+        if(suma>mayor):
+            mayor = suma
     return mayor
         
 print(maxSubArray(nums))
